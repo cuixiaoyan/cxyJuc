600aba903337fbce24096ced1421eecaf4d081cb
cuixiaoyan <56696508@qq.com>
2020-09-21 17:48:43 +0800

    jvm分析

---
+++ src/main/java/com/cxy/jvm/StackStruTest.java
@@ -0,0 +1,28 @@
+package com.cxy.jvm;
+
+/**
+ * @program: cxyJuc
+ * @description:
+ * @author: cuixy
+ * @create: 2020-09-21 15:11
+ **/
+public class StackStruTest {
+
+    private static int num = 1;
+
+    static {
+        num = 2;
+        number = 20;
+        System.out.println(num);
+//        System.out.println(number);  //报错，非法的前向引用
+    }
+
+    private static int number = 10;
+
+
+    public static void main(String[] args) {
+        System.out.println(StackStruTest.num); // 2
+        System.out.println(StackStruTest.number); // 10
+    }
+
+}
